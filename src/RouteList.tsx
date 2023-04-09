3d3275cbfc7359a40965e761581b3ded5eda30d2
YoonJihwan <jecgdd@naver.com>
2023-04-09 22:43:31 +0900

    * emotion 설치 * emotion: 파일마다 pragma를 상단에 작성하지 않아도 되도록 설정

--- src/RouteList.tsx
+++ src/RouteList.tsx
@@ -1,10 +1,15 @@
 import { Routes, Route } from "react-router-dom";
+import { css } from "@emotion/react";
+
+const test = css`
+  background: red;
+`;
 
 const RouteList = () => {
   return (
     <Routes>
       <Route path="/">
-        <Route index element={<div>hello world</div>} />
+        <Route index element={<div css={test}>hello world</div>} />
         <Route path="/hello" element={<div>hello</div>} />
       </Route>
     </Routes>
