60ab4e710a9ea475ef35b7523a0a34c5523aec15
YoonJihwan <jecgdd@naver.com>
2023-04-09 14:08:50 +0900

    * dayjs, timezone setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// dayjs
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+
 import App from "./App";
+
+dayjs.extend(timezone);
+dayjs.tz.setDefault("Asia/Seoul"); // 타임존 설정
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
