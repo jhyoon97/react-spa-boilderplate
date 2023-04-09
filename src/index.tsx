19e326d240ac5cd533ae625cc43463b17790de18
YoonJihwan <jecgdd@naver.com>
2023-04-09 14:12:24 +0900

    * axios, interceptors

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,15 @@ import ReactDOM from "react-dom/client";
 import dayjs from "dayjs";
 import timezone from "dayjs/plugin/timezone";
 
+// utils
+import setAxiosInterceptors from "utils/setAxiosInterceptors";
+
 import App from "./App";
 
 dayjs.extend(timezone);
 dayjs.tz.setDefault("Asia/Seoul"); // 타임존 설정
+
+setAxiosInterceptors();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
